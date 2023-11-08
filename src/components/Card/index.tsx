0ce35ba76bb9339337040f9bfdff354fecb9f84a
RichardPickman <drezzerock@gmail.com>
2023-11-08 13:32:26 +0200

    add id to Card for optimization purposes

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,16 +6,18 @@ import { cn } from '../../utils';
 type Props = {
     children: ReactNode;
     className?: string;
+    id?: number;
     onClick?: (event: ClickEvent) => void;
 } & MotionProps;
 
-export const Card = ({ children, className, onClick, ...props }: Props) => {
+export const Card = ({ children, className, onClick, id, ...props }: Props) => {
     return (
         <m.div
             className={cn(
                 'aspect-video w-full rounded border bg-slate-600',
                 className,
             )}
+            id={String(id)}
             onClick={onClick}
             {...props}
         >
